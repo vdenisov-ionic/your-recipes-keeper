af9a0935840c61311ae902e12df05868986d7d1e
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-12-22 22:01:58 +0300

    06. Add alert for deleting and separate recipe component

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
 
@@ -15,11 +16,15 @@ export class RecipeDetailPage implements OnInit, OnDestroy {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private recipesService: RecipesService,
+    private alertController: AlertController,
   ) { }
 
   ngOnInit() {
     console.warn('[recipe-detail] OnInit');
+
     const recipeId = this.activatedRoute.snapshot.paramMap.get('recipeId');
+    if (!recipeId) { this.router.navigate(['/recipes']); }
+
     this.loadedRecipe = this.recipesService.getRecipe(recipeId);
   }
 
@@ -28,8 +33,22 @@ export class RecipeDetailPage implements OnInit, OnDestroy {
   }
 
   onDeleteRecipe() {
+    this.alertController
+      .create({
+        header: 'Are you sure?',
+        message: 'Do you really want to delete the recipe?',
+        buttons: [
+          { text: 'Cancel', role: 'cancel' },
+          { text: 'Delete', handler: () => this._deleteRecipe() }
+        ],
+      })
+      .then(alertEl => {
+        alertEl.present();
+      });
+  }
+
+  private _deleteRecipe() {
     this.recipesService.deleteRecipe(this.loadedRecipe.id);
     this.router.navigate(['/recipes']);
   }
-
 }
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { RecipesPageRoutingModule } from './recipes-routing.module';
 
 import { RecipesPage } from './recipes.page';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RecipesPageRoutingModule
+    RecipesPageRoutingModule,
   ],
-  declarations: [RecipesPage]
+  declarations: [
+    RecipesPage,
+    RecipeItemComponent,
+  ],
 })
 export class RecipesPageModule {}
