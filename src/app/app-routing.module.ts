a8393c3f533ceced4b5379b25ba35afe22fad038
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-12-22 20:26:40 +0300

    03. Make page for displaying recipe details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'recipes',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+
   {
     path: 'recipes',
-    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule)
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesPageModule),
+      },
+      {
+        path: ':recipeId',
+        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then(m => m.RecipeDetailPageModule),
+      },
+    ],
   },
 ];
 
---
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.page.html',
+  styleUrls: ['./recipe-detail.page.scss'],
+})
+export class RecipeDetailPage implements OnInit {
+  loadedRecipe: Recipe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService,
+  ) { }
+
+  ngOnInit() {
+    const recipeId = this.activatedRoute.snapshot.paramMap.get('recipeId');
+    this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+  }
+
+}
