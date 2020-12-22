be96496c5ff9735945347328cf9731af126d177c
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-12-22 20:02:44 +0300

    02. Add simple state management for recipes

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,24 +8,11 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl: 'https://via.placeholder.com/100x100.png?text=ABC',
-      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://via.placeholder.com/100x100.png?text=123',
-      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'],
-    }
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
-
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: '/assets/img/schnitzel.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: '/assets/img/spaghetti.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'],
+    }
+  ];
+
+  constructor() {}
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })};
+  }
+}
